2a917d6da8a68709e7d89006c87b292031431376
mohamedmbarki <167710486+MohammedMbarki@users.noreply.github.com>
2024-12-09 18:49:13 +0100

    Create src.c

---
+++ SumTwoNumbers /src.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int number1; 
+    int number2;
+
+    printf("Please enter the first number:   ");
+    scanf("%d", &number1); 
+
+    printf("Please enter the second number:   ");
+    scanf("%d", &number2); 
+
+    printf("The sum of the two numbers is: %d\n", number1 + number2); 
+    
+    return 0; 
+}
